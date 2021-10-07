c27704512be4d727e19e4047bd302813f685c886
Jocelyn Tandrea <jocelyntandrea@gmail.com>
2021-10-07 12:28:48 -0700

    Add WelcomeActivity
    Implement setOnClickListener on the skipBtn to start MainActivity

---
+++ app/src/main/java/ca/cmpt276/as3/WelcomeActivity.java
@@ -0,0 +1,31 @@
+package ca.cmpt276.as3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        setupSKipButton();
+    }
+
+    private void setupSKipButton() {
+        Button button = findViewById(R.id.btnSkip);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
