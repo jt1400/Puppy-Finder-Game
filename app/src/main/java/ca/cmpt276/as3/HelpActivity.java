596003ffbdb384e8a6781930d2de5aaf546e15f7
Nguyen Ai Vy (Ivy) Tran <natran@sfu.ca>
2021-10-09 20:56:35 -0700

    Implement Help Screen.
    Add app bar to display Help Title.
    Add game instructions, authors, hyperlink to cmpt276 course website, and add links for image resources.

--- app/src/main/java/ca/cmpt276/as3/HelpActivity.java
+++ app/src/main/java/ca/cmpt276/as3/HelpActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -12,6 +13,28 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        fillOutHelpContent();
+    }
+
+    private void fillOutHelpContent()
+    {
+        TextView tvGameIns = findViewById(R.id.textViewGameInstructionContent);
+        String gameIns = "To find a hidden puppy, you can click on any of the cell on your game screen. " +
+                "If the cell happens to contain a hidden puppy, then congrats, you have just found one more puppy." +
+                "If the cell does not contain a hidden puppy, then it will show you the number of hidden puppies left on that row and column. "+
+                "You can also click on a puppy to trigger the scan on its row and column for hidden puppies.";
+        tvGameIns.setText(gameIns);
+
+        TextView tvAuthor = findViewById(R.id.textViewAuthorContent);
+        String author = "Written by Jocelyn Tandrea and Ivy Tran for Introduction to Software Engineering. "+
+                "Course website: https://opencoursehub.cs.sfu.ca/bfraser/grav-cms/cmpt276/home ";
+        tvAuthor.setText(author);
+
+        TextView tvResources = findViewById(R.id.textViewResourcesContent);
+        String resource = "Image of Puppies and Books by Iryna Kazlova Airspa " +
+                "(https://www.dreamstime.com/cute-little-puppies-shelf-books-light-background-pembroke-welsh-corgi-closeup-photo-image140177115 )";
+        tvResources.setText(resource);
     }
 
     public static Intent makeIntent(Context context)
